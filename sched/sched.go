0fb6c6c5aad0882c44d352a779cf5295cf53e82a
vlaner <vlaner@bk.ru>
2025-03-30 17:48:37 +0300

    add store for tasks

--- sched/sched.go
+++ sched/sched.go
@@ -26,6 +26,8 @@ type Scheduler struct {
 
 	scheduleSignal chan struct{}
 
+	store TaskStore
+
 	ctx    context.Context
 	cancel context.CancelFunc
 }
@@ -36,9 +38,17 @@ func defaultErrorHandler(task *Task, err error) {
 
 type HandlerFunc func(context.Context, any) error
 
-func NewScheduler(workers int) *Scheduler {
+type SchedulerOpt func(*Scheduler)
+
+func WithStore(store TaskStore) SchedulerOpt {
+	return func(s *Scheduler) {
+		s.store = store
+	}
+}
+
+func NewScheduler(workers int, opts ...SchedulerOpt) *Scheduler {
 	ctx, cancel := context.WithCancel(context.Background())
-	return &Scheduler{
+	s := Scheduler{
 		handlers:       make(map[string]HandlerFunc),
 		workers:        workers,
 		wg:             sync.WaitGroup{},
@@ -49,6 +59,16 @@ func NewScheduler(workers int) *Scheduler {
 		ctx:            ctx,
 		cancel:         cancel,
 	}
+
+	for _, o := range opts {
+		o(&s)
+	}
+
+	if s.store == nil {
+		s.store = NewMemoryStore()
+	}
+
+	return &s
 }
 
 func (s *Scheduler) Start() {
@@ -99,15 +119,20 @@ func (s *Scheduler) Register(taskName string, handler HandlerFunc) error {
 }
 
 func (s *Scheduler) AddTask(task Task) error {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
+	s.mu.RLock()
 	_, exists := s.handlers[task.Name]
 	if !exists {
 		return errors.New("handler does not exist")
 	}
+	s.mu.RUnlock()
 
-	s.tasks[task.ID] = &task
+	if task.NextRunAt.IsZero() {
+		task.NextRunAt = time.Now().UTC()
+	}
+	err := s.store.Save(s.ctx, &task)
+	if err != nil {
+		return err
+	}
 
 	s.signalSchedule()
 
@@ -126,22 +151,7 @@ func (s *Scheduler) scheduleLoop() {
 	defer timer.Stop()
 
 	for {
-		var hasTasks bool
-		var earliestNextRun time.Time
-
-		s.mu.RLock()
-		for _, task := range s.tasks {
-			if task.Status == PENDING || task.Status == RESCHEDULED {
-				hasTasks = true
-				if earliestNextRun.IsZero() {
-					earliestNextRun = task.NextRunAt
-				}
-				if task.NextRunAt.Before(earliestNextRun) {
-					earliestNextRun = task.NextRunAt
-				}
-			}
-		}
-		s.mu.RUnlock()
+		earliestNextRun, hasTasks := s.store.EarliestRun(s.ctx)
 
 		// block run loop until has tasks, without consuming CPU power
 		var waitDuration time.Duration = math.MaxInt64
@@ -163,27 +173,37 @@ func (s *Scheduler) scheduleLoop() {
 			continue
 		case now := <-timer.C:
 			now = now.UTC()
-			var tasksToRun []*Task
 
-			s.mu.Lock()
-			for _, task := range s.tasks {
-				if now.After(task.NextRunAt) && (task.Status == PENDING || task.Status == RESCHEDULED) {
-					task.Status = RUNNING
-					task.StartedAt = &now
-					tasksToRun = append(tasksToRun, task)
-				}
+			tasksToRun, err := s.store.Pending(s.ctx, now)
+			if err != nil {
+				log.Println("get pending tasks: ", err)
+				continue
 			}
-			s.mu.Unlock()
 
 			for _, task := range tasksToRun {
+				updErr := s.store.Update(s.ctx, task.ID, func(t *Task) error {
+					task.Status = RUNNING
+					task.StartedAt = &now
+					return nil
+				})
+				if updErr != nil {
+					s.errorHandler(task, fmt.Errorf("scheduled task update: %w", err))
+					// TODO: revert task state?
+				}
+
 				select {
 				case s.taskCh <- task:
 				default:
 					log.Println("could not send task to process", task)
-					s.mu.Lock()
-					task.Status = PENDING
-					task.StartedAt = nil
-					s.mu.Unlock()
+					updErr = s.store.Update(s.ctx, task.ID, func(t *Task) error {
+						task.Status = PENDING
+						task.StartedAt = nil
+						return nil
+					})
+					if updErr != nil {
+						s.errorHandler(task, fmt.Errorf("scheduled not sent task update: %w", err))
+					}
+
 				}
 			}
 
@@ -205,12 +225,11 @@ func (s *Scheduler) worker(ctx context.Context, id int) {
 }
 
 // TODO: improve synchronization
-func (s *Scheduler) handleTask(ctx context.Context, workerID int, task *Task) {
-	s.mu.RLock()
-	task, exists := s.tasks[task.ID]
-	s.mu.RUnlock()
-
-	if !exists {
+func (s *Scheduler) handleTask(ctx context.Context, workerID int, t *Task) {
+	task, err := s.store.GetByID(s.ctx, t.ID)
+	if err != nil {
+		// task from Store not found, use one passed to function
+		s.errorHandler(t, fmt.Errorf("get task: %w", err))
 		return
 	}
 
@@ -224,29 +243,43 @@ func (s *Scheduler) handleTask(ctx context.Context, workerID int, task *Task) {
 	handleCtx, handleCancel := context.WithCancel(ctx)
 	defer handleCancel()
 
-	err := handler(handleCtx, task.Payload)
+	err = handler(handleCtx, task.Payload)
 	now := time.Now().UTC()
 
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	task.End(now)
+	updErr := s.store.Update(s.ctx, task.ID, func(t *Task) error {
+		t.End(now)
+		return nil
+	})
+	if updErr != nil {
+		s.errorHandler(task, fmt.Errorf("task update: %w", err))
+	}
 
 	if err != nil {
 		if task.MaxRetries > 0 && task.Retries < task.MaxRetries {
-			task.Reschedule()
-			s.tasks[task.ID] = task
+			updErr := s.store.Update(s.ctx, task.ID, func(t *Task) error {
+				t.Reschedule()
+				return nil
+			})
+			if updErr != nil {
+				s.errorHandler(task, fmt.Errorf("retry task update: %w", err))
+			}
 			return
 		} else {
 			s.errorHandler(task, fmt.Errorf("max retries reached: %w", err))
 		}
 	}
 
-	delete(s.tasks, task.ID)
+	delErr := s.store.Delete(s.ctx, task.ID)
+	if delErr != nil {
+		s.errorHandler(task, fmt.Errorf("task delete: %w", err))
+	}
 
 	if task.Recurring {
 		newTask := task.copy()
-		s.tasks[newTask.ID] = newTask
+		saveErr := s.store.Save(s.ctx, newTask)
+		if saveErr != nil {
+			s.errorHandler(newTask, fmt.Errorf("recurring task save: %w", err))
+		}
 	}
 
 }
---
+++ sched/store.go
@@ -0,0 +1,166 @@
+package sched
+
+import (
+	"context"
+	"errors"
+	"sync"
+	"time"
+)
+
+var ErrTaskNotFound = errors.New("task not found")
+
+type TaskStore interface {
+	Save(ctx context.Context, task *Task) error
+	GetByID(ctx context.Context, ID string) (*Task, error)
+	Delete(ctx context.Context, ID string) error
+	Update(ctx context.Context, taskID string, updateFn func(*Task) error) error
+
+	Pending(ctx context.Context, now time.Time) ([]*Task, error)
+	EarliestRun(ctx context.Context) (time.Time, bool)
+	MarkStatus(ctx context.Context, status TaskStatus, ids ...string) error
+}
+
+type MemoryStore struct {
+	tasks map[string]*Task
+	mu    sync.RWMutex
+}
+
+func NewMemoryStore() *MemoryStore {
+	return &MemoryStore{
+		tasks: make(map[string]*Task),
+		mu:    sync.RWMutex{},
+	}
+}
+
+func (m *MemoryStore) Save(ctx context.Context, task *Task) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.tasks[task.ID] = task
+
+	return nil
+}
+
+func (m *MemoryStore) GetByID(ctx context.Context, ID string) (*Task, error) {
+	m.mu.RLock()
+	task, exists := m.tasks[ID]
+	m.mu.RUnlock()
+
+	if !exists {
+		return nil, ErrTaskNotFound
+	}
+
+	return task, nil
+}
+
+func (m *MemoryStore) Delete(ctx context.Context, ID string) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	delete(m.tasks, ID)
+
+	return nil
+}
+
+func (m *MemoryStore) EarliestRun(ctx context.Context) (time.Time, bool) {
+	var hasTasks bool
+	var earliestNextRun time.Time
+
+	m.mu.RLock()
+	for _, task := range m.tasks {
+		if task.Status == PENDING || task.Status == RESCHEDULED {
+			hasTasks = true
+			if earliestNextRun.IsZero() {
+				earliestNextRun = task.NextRunAt
+			}
+			if task.NextRunAt.Before(earliestNextRun) {
+				earliestNextRun = task.NextRunAt
+			}
+		}
+	}
+	m.mu.RUnlock()
+
+	return earliestNextRun, hasTasks
+}
+
+func (m *MemoryStore) Pending(ctx context.Context, now time.Time) ([]*Task, error) {
+	var tasksToRun []*Task
+
+	m.mu.Lock()
+	for _, task := range m.tasks {
+		if now.After(task.NextRunAt) && (task.Status == PENDING || task.Status == RESCHEDULED) {
+			tasksToRun = append(tasksToRun, task)
+		}
+	}
+	m.mu.Unlock()
+
+	return tasksToRun, nil
+}
+
+func (m *MemoryStore) MarkStatus(ctx context.Context, status TaskStatus, ids ...string) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	now := time.Now().UTC()
+
+	for _, id := range ids {
+		task, exists := m.tasks[id]
+		if !exists {
+			continue
+		}
+		task.Status = status
+
+		switch status {
+		case RUNNING:
+			task.StartedAt = &now
+		case FINISHED:
+			task.EndedAt = &now
+		case PENDING:
+			task.StartedAt = nil
+			task.EndedAt = nil
+		}
+	}
+
+	return nil
+}
+
+func (m *MemoryStore) Update(ctx context.Context, taskID string, updateFn func(*Task) error) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	task, exists := m.tasks[taskID]
+	if !exists {
+		return ErrTaskNotFound
+	}
+
+	err := updateFn(task)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *MemoryStore) End(ctx context.Context, taskID string, at time.Time) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	task, exists := m.tasks[taskID]
+	if !exists {
+		return ErrTaskNotFound
+	}
+
+	task.End(at)
+
+	return nil
+}
+func (m *MemoryStore) Reschedule(ctx context.Context, taskID string) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	task, exists := m.tasks[taskID]
+	if !exists {
+		return ErrTaskNotFound
+	}
+
+	task.Reschedule()
+
+	return nil
+}
